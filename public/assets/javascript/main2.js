6a227b89e2c586fec1344001a1f0a2653625567e
StarfoxMulder <cjprestia@gmail.com>
2019-08-09 15:03:50 -0400

    modal for project info working on JP Development, need to expand to rest. Added About section, updated home anchor to About, need to flesh out that section

--- public/assets/javascript/main2.js
+++ public/assets/javascript/main2.js
@@ -6,11 +6,25 @@ $(document).ready(function () {
   $("#ViewMyWork").click(function (e) {
     e.preventDefault();
     $('html,body').animate({
-        scrollTop: $("#portfolio").offset().top
+        scrollTop: $("#aboutMe").offset().top
       },
       'slow');
   });
 
+  $('.projItem').click(function (event) {
+    event.preventDefault();
+    var e = $(this);
+    var title = e.data('title');
+    var tech = e.data('tech');
+    var about = e.data('about');
+    var url = e.data('url');
+
+    $("#myModal").modal("show");
+    $('.projTitleModal').html(title);
+    $('.projTechModal').html(tech);
+    $('.projAboutModal').html(about);
+    $('#projVisitModal').attr("href", url);
+  });
 
   function getCookie(cookiename) {
     var cookiestring = RegExp("" + cookiename + "[^;]+").exec(document.cookie);
