ebcb05478620c3cd1f89c8653a890eafa6423953
StarfoxMulder <cjprestia@gmail.com>
2019-08-09 11:19:57 -0400

    much better layout, styling on track, some hover transitions

--- public/assets/javascript/main2.js
+++ public/assets/javascript/main2.js
@@ -3,11 +3,14 @@ $(document).ready(function () {
   // var popupHtml = '';
   // $("body").append(popupHtml);
 
-  $("#myModal").modal()
-
-  $("#closeIcon").click(function () {
-    $("#myModal").css("display", "none");
+  $("#ViewMyWork").click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $("#portfolio").offset().top
+      },
+      'slow');
   });
+
 
   function getCookie(cookiename) {
     var cookiestring = RegExp("" + cookiename + "[^;]+").exec(document.cookie);
